429cd7c2547fa52fcb9583484e8ca77bac381471
roky18 <rokymax66@gmail.com>
2025-08-25 23:50:16 +0600

    3rd update

--- main.js
+++ main.js
@@ -48,36 +48,69 @@ document.getElementById("withdraw-Btn").addEventListener("click", function (e) {
   let newAvBalence = AvlBlance - withdraw;
   document.getElementById("ToAmount").innerText = newAvBalence;
 });
+
+//function to toggle//
+function handleToggle(id) {
+  let allSections = document.getElementsByClassName("allSection");
+
+  for (let allSection of allSections) {
+    allSection.style.display = "none";
+  }
+
+  document.getElementById(id).style.display = "block";
+}
+
+//fungction to buttone handle//
+
+function handleBtn(id) {
+  let formBtn = document.getElementsByClassName("form-btn");
+  for (let btn of formBtn) {
+    btn.classList.remove("border-[#0874f2]", "bg-[#0874f20d]");
+    btn.classList.add("border-gray-300");
+  }
+
+  document.getElementById(id).classList.remove("border-gray-300");
+  document
+    .getElementById(id)
+    .classList.add("border-[#0874f2]", "bg-[#0874f20d]");
+}
 // toggling feature---
 
 document.getElementById("addMoney-Btn").addEventListener("click", function () {
-  let allSections = document.getElementsByClassName("allSection");
-
-  for (let allSection of allSections) {
-    allSection.style.display = "none";
-  }
-
-  document.getElementById("addMoney").style.display = "block";
+  handleToggle("addMoney");
+  handleBtn("addMoney-Btn");
 });
 document.getElementById("cashOut-Btn").addEventListener("click", function () {
-  let allSections = document.getElementsByClassName("allSection");
-
-  for (let allSection of allSections) {
-    allSection.style.display = "none";
-  }
-
-  document.getElementById("cashOut").style.display = "block";
+  handleToggle("cashOut");
+  handleBtn("cashOut-Btn");
 });
+
+// transfer butone--->
 
 document.getElementById("Transfer-Btn").addEventListener("click", function () {
-  let allSections = document.getElementsByClassName("allSection");
+  handleToggle("transMoney");
+  handleBtn("Transfer-Btn");
+});
 
-  for (let allSection of allSections) {
-    allSection.style.display = "none";
+let wPinTf = 9090;
+document.getElementById("sendBtn").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  let AgentNumberTTf = document.getElementById("AgentNbrTf").value;
+  let withdraPinTTf = parseInt(document.getElementById("withdrawPinTf").value);
+
+  if (AgentNumberTTf.length !== 11) {
+    alert("Please Provide A Valide Acount Number");
+    return;
   }
 
-  document.getElementById("transMoney").style.display = "block";
+  // prrrrooooooobbbbbbbllllllllleeeeem--
+  if (withdraPinTTf !== wPinTf) {
+    alert("Please Provide A Valide pin Number");
+    return;
+  }
 });
+// bonuse-->
 document.getElementById("Bonus").addEventListener("click", function () {
   let allSections = document.getElementsByClassName("allSection");
 
@@ -86,4 +119,19 @@ document.getElementById("Bonus").addEventListener("click", function () {
   }
 
   document.getElementById("bonusMny").style.display = "block";
+
+  handleBtn("Bonus");
+});
+
+// pay bill--->
+
+document.getElementById("PayBillBtn").addEventListener("click", function () {
+  handleToggle("billpay");
+  handleBtn("PayBillBtn");
+});
+// Transction--->
+
+document.getElementById("TrnBtn").addEventListener("click", function () {
+  handleToggle("Transactions");
+  handleBtn("TrnBtn");
 });
